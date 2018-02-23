9a7be6b8553e1f51622ff65ba0228369dd075eea
Changdao <jameslau@263.net>
2018-02-23 21:50:55 +0800

    initing test

---
+++ test/unit.test.mocha.spec.js
@@ -0,0 +1,13 @@
+var tinychain = require('../tinychain');
+var chai = require('chai');
+var expect = chai.expect;
+
+
+describe('tinychain.js',function(){
+	describe('utils',function(){
+		it('shoul do right sha256d',function(){
+			//hash256d('123') should equals 5a77d1e9612d350b3734f6282259b7ff0a3f87d62cfef5f35e91a5604c0490a3;
+			expect(tinychain.sha256d('123')).to.equal('5a77d1e9612d350b3734f6282259b7ff0a3f87d62cfef5f35e91a5604c0490a3');
+		});
+	});
+});
