0bd656fad89a60ac4cf8d7e0c307902f092df533
Changdao <jameslau@263.net>
2018-02-23 22:20:30 +0800

    fix finger slip

--- test/unit.test.mocha.spec.js
+++ test/unit.test.mocha.spec.js
@@ -5,7 +5,7 @@ var expect = chai.expect;
 
 describe('tinychain.js',function(){
 	describe('utils',function(){
-		it('shoul do right sha256d',function(){
+		it('should do right sha256d',function(){
 			//hash256d('123') should equals 5a77d1e9612d350b3734f6282259b7ff0a3f87d62cfef5f35e91a5604c0490a3;
 			expect(tinychain.sha256d('123')).to.equal('5a77d1e9612d350b3734f6282259b7ff0a3f87d62cfef5f35e91a5604c0490a3');
 		});
